4f6b7f4e054bea5a83e9c0c4ba14cc8cc5b356f3
Desconocido502 <3018554470101@ingenieria.usac.edu.gt>
2023-03-25 00:01:21 -0600

    segundo commit

---
+++ main2.py
@@ -0,0 +1,11 @@
+edadMarco = 18
+edadCarlos = 21
+
+if edadMarco <= 17:
+    print("Marco es mayor de edad")
+elif edadCarlos <= 18:
+    print("Carlos es mayor de edad")
+elif edadMarco <= edadCarlos:
+    print("Marco es menor que Carlos")
+
+print(edadCarlos > edadMarco)
