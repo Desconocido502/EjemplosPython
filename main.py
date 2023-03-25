6114e7c0421dee2dcad0efdd671502ca6e743bc0
Desconocido502 <3018554470101@ingenieria.usac.edu.gt>
2023-03-24 23:39:42 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print('Hola Mundo')
+
+msg = input('Escribe un numero: ')
+
+if(int(msg) == 1):
+    print('Lunes')
+elif (int(msg) == 2):
+    print('Martes')
+elif (int(msg) == 3):
+    print('Miercoles')
+elif (int(msg) == 4):
+    print('Jueves')
+elif (int(msg) == 5):
+    print('Viernes')
+elif (int(msg) == 6):
+    print('Sabado')
+elif (int(msg) == 7):
+    print('Domingo')
+else:
+    print('No es un dia de la semana')
